476f4fd95f841d7b7c7c53c69a508a1db3099145
lamb <lamb@UTFPRMD-408047.MD.UTFPR>
2022-10-18 08:46:35 -0300

    Segundo commit: foi adicionada uma nova funcionalidade de exibição de mensagem

--- src/main/java/br/edu/utfpr/exemploversionamento/ExemploVersionamento.java
+++ src/main/java/br/edu/utfpr/exemploversionamento/ExemploVersionamento.java
@@ -13,5 +13,6 @@ public class ExemploVersionamento {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Nova mensagem");
     }
 }
